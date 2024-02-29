9872d31c3d477f0b7756e2481d18582e3218d1ba
gabrielmitak <mitak.gabriel@zst.radom.pl>
2024-02-29 13:09:29 +0100

    files

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+using Newtonsoft.Json.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        WebClient client = new WebClient();
+        string kursWymianyString = client.DownloadString("https://api.exchangerate-api.com/v4/latest/PLN");
+
+        JObject jsonObj = JObject.Parse(kursWymianyString);
+        double kursWymiany = (double)jsonObj["rates"]["USD"];
+
+        Console.Write("Wprowadź kwotę w PLN: ");
+        double kwotaPLN = Convert.ToDouble(Console.ReadLine());
+
+        double kwotaUSD = kwotaPLN * kursWymiany;
+
+        Console.WriteLine("Kwota w USD: " + kwotaUSD);
+    }
+}
